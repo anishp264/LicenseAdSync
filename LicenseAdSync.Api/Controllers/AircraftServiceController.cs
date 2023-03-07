c035cfe697201e159775cf70d2a50fd6fc1cd044
anishp264 <punnenanish@gmail.com>
2023-03-07 22:17:37 +0200

    Initial Commit

---
+++ LicenseAdSync.Api/Controllers/AircraftServiceController.cs
@@ -0,0 +1,17 @@
+﻿using LicenseAdSync.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LicenseAdSync.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AircraftServiceController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            AircraftService aircraftService = new AircraftService();
+            return aircraftService.GetAircraft();
+        }
+    }
+}
---
+++ LicenseAdSync.Domain/Services/AircraftService.cs
@@ -0,0 +1,17 @@
+﻿using LicenseAdSync.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseAdSync.Domain.Services
+{
+    public class AircraftService : IAircraft
+    {
+        public string GetAircraft()
+        {
+            return "B787-10 Dreamliner";
+        }
+    }
+}
